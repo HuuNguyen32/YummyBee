564021141529fe6137297e0acca04ceb20ddb8af
HuuNguyen32 <nhn322004@gmail.com>
2025-11-11 07:23:53 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -37,7 +38,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Thư viện tải ảnh (Rất quan trọng để hiển thị ảnh từ Cloudinary URL)
+    implementation(libs.glide)
+    annotationProcessor(libs.compiler)
+
+    implementation(libs.material.v1110)
 }
